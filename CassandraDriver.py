334e48ca3e9d1c7a980951025bbbc5a8676c9f7d
jafarbadour <jafer.ba@gmail.com>
2019-04-13 22:50:12 +0300

    added plotting

--- CassandraDriver.py
+++ CassandraDriver.py
@@ -1,9 +1,14 @@
 import coloredlogs, logging
+from mpl_toolkits.mplot3d import Axes3D
+
 from Randomizer import randomizeData
 from create_tables import create_session
 import matplotlib.pyplot as plt
 from datetime import date
 import math
+from mpl_toolkits import mplot3d
+import matplotlib.pyplot as plt
+import numpy as np
 
 coloredlogs.install()
 
@@ -18,12 +23,13 @@ class CassandraDriver:
     def __init__(self, flag=True):  # Flag is sat to true if user wants to call randomizer
         cluster = Cluster()
         self.session = cluster.connect()
+        self.plot_array = []
         print("Creating tables here please wait and be patient ...")
         create_session(session=self.session)
         print("Table creation was successful")
         if flag:
             print("Flag was sat to True Initiating randomizing data please wait for ~2 minute")
-            randomizeData(session=self.session, MAX=100)
+            randomizeData(session=self.session, MAX=10)
         else:
             print("Flag was sat to False there will be no randomizing data")
 
@@ -114,6 +120,12 @@ class CassandraDriver:
         normalized_array = [[] for i in range(N)]
         students_list = []
         counter = 0
+        if plot_flag == True and len(list_of_subjects) != 2:
+            print("You cannot plot if you have more than 3 dimensions, age, subj1, subj2")
+            assert 0
+        X = []
+        Y = []
+        Z = []
         for i in rows:
             sid = i[4]
             name = i[0]
@@ -128,6 +140,19 @@ class CassandraDriver:
                 normalized_array[ind].append([i[2], i[3]])
             else:
                 normalized_array[ind].append([i[2], i[3]])
+
+        for i in normalized_array:
+            if len(i) == 0:
+                break
+            for j in range(3, len(i)):
+                element = i[j]
+                if element[0] == list_of_subjects[0]:
+                    X.append(element[1])
+                elif element[0] == list_of_subjects[1]:
+                    Y.append(element[1])
+            Z.append(i[1])
+        self.plot_array = [X, Y, Z]
+
         for st1 in normalized_array:
             if len(st1) == 0:
                 break
@@ -154,6 +179,17 @@ class CassandraDriver:
 
         self.session.execute(query)
 
+    def show_graph(self, list_of_subjects):
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        # Data for a three-dimensional line
+        X, Y, Z = self.plot_array
+        ax.scatter(X, Y, Z, c='r', marker='o')
+        ax.set_xlabel(list_of_subjects[0] + " grade")
+        ax.set_ylabel(list_of_subjects[1] + " grade")
+        ax.set_zlabel("age")
+        plt.show()
+
 
 def showdata(data):
     for row in data:
@@ -163,7 +199,7 @@ def showdata(data):
 obj = CassandraDriver(flag=False)
 
 rowData = obj.geospacial_search_get(['Arabic', 'Math'], [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18])
-
+obj.show_graph(['Arabic', 'Math'])
 # data = obj.students_in_birthdate_range('1900-10-10', '2010-01-01')
 # showdata(data)
 # print(data)
