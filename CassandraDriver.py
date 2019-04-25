dd1d2b196bf8f4d68b450e67cbda5a3f7e2dabb2
jafarbadour <jafer.ba@gmail.com>
2019-04-26 01:21:38 +0300

    Merge branch 'dev' of https://github.com/Akhmetbekali/-S19-DMD-II
    # Conflicts:
    #	CassandraDriver.py
    
    added also Histogram stuff

--- CassandraDriver.py
+++ CassandraDriver.py
@@ -261,9 +261,9 @@ def showdata(data):
 
 
 t1 = time.time()
-obj = CassandraDriver(flag=True)
+obj = CassandraDriver(flag=False)
 
-rowData = obj.geospacial_search_get('6b57c478-0852-4719-a543-e748940fd54d', ['Arabic', 'Math'],
+rowData = obj.geospacial_search_get('2834cae4-7e3c-4d28-9e59-568b3e7fe4ee', ['Arabic', 'Math'],
                                     [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18])
 obj.show_graph(['Arabic', 'Math'])
 
